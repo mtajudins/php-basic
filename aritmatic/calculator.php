68d026084f0731fb363bd4c9efefde7924327f17
unknown <mtajudin98@gmail.com>
2019-04-28 21:21:43 +0700

    First Commit:

---
+++ aritmatic/calculator.php
@@ -0,0 +1,25 @@
+<?php
+echo "Penjumlahan dari : <br>";
+$a = 10;
+$b = 20;
+$hasil = $a+$b;
+echo $hasil."<br>";
+
+echo "Pengurangan dari :<br>";
+$a1 = 10;
+$b1 = 20;
+$hasil1 = $a-$b;
+echo $hasil."<br>";
+
+echo "Perkalian dari : <br>";
+$a2 = 10;
+$b2 = 20;
+$hasil2 = $a*$b;
+echo $hasil2."<br>";
+
+echo "Pembagian  dari :<br>";
+$a3 = 10;
+$b3 = 20;
+$hasil3 = $a/$b;
+echo $hasil3."<br>";
+?>
